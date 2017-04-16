4c722c34ab4d3240ebc5796c2bd9a4c461675a34
johnzw <rzzw456@126.com>
2017-04-16 16:28:28 -0700

    move the env variable to another file

---
+++ configure_env.py
@@ -0,0 +1,5 @@
+PRJ_ROOT = "/Users/john/Desktop/dev/tempSave524/"
+LOG_FILE_PATH = "/Users/john/Desktop/dev/tempSave524/log"
+WAVE_FILE_PATH = "/Users/john/Desktop/dev/tempSave524/given_path.xml"
+TMP_DF_PATH = "/Users/john/Desktop/dev/tempSave524/tmp_df"
+GAMETABLE_CSV_FILE = '/Users/john/Desktop/dev/tempSave524/gametable.csv'
--- loopControl.py
+++ loopControl.py
@@ -2,13 +2,7 @@ import os
 #import datetime
 import random
 import evolution_algorithm
-
-PRJ_ROOT = "/Users/john/Desktop/dev/tempSave524/"
-
-LOG_FILE_PATH = "/Users/john/Desktop/dev/tempSave524/log"
-WAVE_FILE_PATH = "/Users/john/Desktop/dev/tempSave524/given_path.xml"
-TMP_DF_PATH = "/Users/john/Desktop/dev/tempSave524/tmp_df"
-GAMETABLE_CSV_FILE = '/Users/john/Desktop/dev/tempSave524/gametable.csv'
+from configure_env import *
 
 def get_info_path(target_folder_path):
     return target_folder_path + "info"
