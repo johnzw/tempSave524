373b84f8df33d9f5b6f294391e59c7208a089f30
oushiff <fanfei@usc.edu>
2017-05-24 18:12:30 -0700

    concise configure file

--- configure_env.py
+++ configure_env.py
@@ -1,7 +1,8 @@
-PRJ_ROOT = "/Users/john/Desktop/dev/tempSave524/pools/"
-LOG_FILE_PATH = "/Users/john/Desktop/dev/tempSave524/sample_log"
-WAVE_FILE_PATH = "/Users/john/Desktop/dev/tempSave524/evolution_algorithm/given_path.xml"
-TMP_DF_PATH = "/Users/john/Desktop/dev/tempSave524/tmp_df"
-GAMETABLE_CSV_FILE = '/Users/john/Desktop/dev/tempSave524/evolution_algorithm/gametable.csv'
-RESULT_PATH = "/Users/john/Desktop/dev/tempSave524/result/final_wave"
-TARGET_PATH = "/Users/john/Desktop/dev/tempSave524/target"
+LOOP_CONTROL_ROOT = "/Users/Franz/Documents/524LoopControl/LoopControl/"
+LOG_FILE_PATH = "/Users/Franz/Library/Logs/Unity/Editor.log"
+PRJ_ROOT = LOOP_CONTROL_ROOT + "pools/"
+WAVE_FILE_PATH = LOOP_CONTROL_ROOT + "evolution_algorithm/given_path.xml"
+TMP_DF_PATH = LOOP_CONTROL_ROOT + "tmp_df"
+GAMETABLE_CSV_FILE = LOOP_CONTROL_ROOT + 'evolution_algorithm/gametable.csv'
+RESULT_PATH = LOOP_CONTROL_ROOT + "result/final_wave"
+TARGET_PATH = LOOP_CONTROL_ROOT + "target"
